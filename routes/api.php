d822f5e2b0de9aaa039e5eeb9c5996e8783436a5
Uffe Petersen <up@mybanker.dk>
2018-05-31 09:08:05 +0200

    Grouping api routes a bit

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::resource('/task', 'TaskController');
-Route::get('/category/{category}/tasks', 'CategoryController@tasks');
-Route::resource('/category', 'CategoryController');
-
 Route::post('login', 'UserController@login');
 Route::post('register', 'UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::resource('/task', 'TaskController');
+    Route::resource('/category', 'CategoryController');
+    Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+}
+
+
